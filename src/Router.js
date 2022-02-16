b6088fcbf4123407b4d903c8cbfdb59042756d0c
Bueong <pcj0308@gmail.com>
2022-02-16 15:45:38 +0900

    Add : initial settings complete

---
+++ src/Router.js
@@ -0,0 +1,36 @@
+import react from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import LoginPark from "./pages/cheoljinPark/Login/Login";
+import MainPark from "./pages/cheoljinPark/Main/Main";
+
+import LoginKim from "./pages/hyejinKim/Login/Login";
+import MainKim from "./pages/hyejinKim/Main/Main";
+
+import LoginLee from "./pages/sangminLee/Login/Login";
+import MainLee from "./pages/sangminLee/Main/Main";
+
+import LoginHan from "./pages/younghyunHan/Login/Login";
+import MainHan from "./pages/younghyunHan/Main/Main";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login-kim" element={<LoginKim />} />
+        <Route path="/main-kim" element={<MainKim />} />
+
+        <Route path="/login-park" element={<LoginPark />} />
+        <Route path="/main-park" element={<MainPark />} />
+
+        <Route path="/login-lee" element={<LoginLee />} />
+        <Route path="/main-lee" element={<MainLee />} />
+
+        <Route path="/login-han" element={<LoginHan />} />
+        <Route path="/main-han" element={<MainHan />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
