834a5f158778d7b478e2f8e71048f170540d07b7
Bueong <pcj0308@gmail.com>
2022-02-18 18:17:02 +0900

    Fix: 주석추가 및 추후 login 기능 구현을 위한 id를 className으로 변경

--- src/pages/cheoljinPark/Login/Login.js
+++ src/pages/cheoljinPark/Login/Login.js
@@ -12,8 +12,12 @@ function Login() {
     <div className="Login">
       <header className="logo">Westagram</header>
       <section className="login-box">
-        <input type="text" placeholder="전화번호, 사용자 이름 또는 이메일" />
-        <input type="password" placeholder="비밀번호" />
+        <input
+          type="text"
+          className="login-id"
+          placeholder="전화번호, 사용자 이름 또는 이메일"
+        />
+        <input type="password" className="login-pw" placeholder="비밀번호" />
         <button className="login-btn" onClick={goToMain}>
           로그인
         </button>
