387b868c099b245cbad6af387636dcff922e66f1
Bueong <pcj0308@gmail.com>
2022-02-18 19:32:10 +0900

    Fix: login 화면 loginbox 폼 태그로 변경 및 css 수정

--- src/pages/cheoljinPark/Login/Login.js
+++ src/pages/cheoljinPark/Login/Login.js
@@ -11,17 +11,21 @@ function Login() {
   return (
     <div className="Login">
       <header className="logo">Westagram</header>
-      <section className="login-box">
+      <form className="login-box">
         <input
           type="text"
-          className="login-id"
+          className="login-id loginInput"
           placeholder="전화번호, 사용자 이름 또는 이메일"
         />
-        <input type="password" className="login-pw" placeholder="비밀번호" />
+        <input
+          type="password"
+          className="login-pw loginInput"
+          placeholder="비밀번호"
+        />
         <button className="login-btn" onClick={goToMain}>
           로그인
         </button>
-      </section>
+      </form>
       <footer className="search-passwd">
         <a href="#">비밀번호를 잊으셨나요?</a>
       </footer>
