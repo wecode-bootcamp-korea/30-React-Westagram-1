dedfaf0b741b657dfd43d95c0430d903f62ba713
Bueong <pcj0308@gmail.com>
2022-02-18 17:39:35 +0900

    Add: Login 파일 업데이트 및 스타일 변경사항 반영

--- src/pages/cheoljinPark/Login/Login.js
+++ src/pages/cheoljinPark/Login/Login.js
@@ -1,7 +1,28 @@
 import './Login.scss';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
-  return <></>;
+  const navigate = useNavigate();
+
+  const goToMain = () => {
+    navigate('/main-park');
+  };
+
+  return (
+    <div className="Login">
+      <header className="logo">Westagram</header>
+      <section className="login-box">
+        <input type="text" placeholder="전화번호, 사용자 이름 또는 이메일" />
+        <input type="password" placeholder="비밀번호" />
+        <button className="login-btn" onClick={goToMain}>
+          로그인
+        </button>
+      </section>
+      <footer className="search-passwd">
+        <a href="#">비밀번호를 잊으셨나요?</a>
+      </footer>
+    </div>
+  );
 }
 
 export default Login;
--- src/pages/cheoljinPark/Main/Main.js
+++ src/pages/cheoljinPark/Main/Main.js
@@ -1,8 +1,19 @@
 import './Main.scss';
 import '../../../styles/variables.scss';
+import Nav from '../../../components/Nav/Nav';
+import Feeds from './Feeds';
+import Aside from './Aside';
 
 function Main() {
-  return <></>;
+  return (
+    <>
+      <Nav />
+      <main className="main">
+        <Feeds />
+        <Aside />
+      </main>
+    </>
+  );
 }
 
 export default Main;
