16274feaab84ac1c3b759762153fb650a790f852
younghyunHan <88773996+younghyunHan@users.noreply.github.com>
2022-02-19 19:51:25 +0900

    Add: Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/younghyunHan/Login/Login.js
+++ src/pages/younghyunHan/Login/Login.js
@@ -1,7 +1,50 @@
 import './Login.scss';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 function Login() {
-  return <></>;
+  const [state, setState] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
+
+  function handleIdInput(event) {
+    if (event.target.type === 'text') {
+      setState(event.target.value);
+    } else if (event.target.type === 'password') {
+      setPasswordValue(event.target.value);
+    }
+  }
+
+  const navigate = useNavigate();
+
+  const goToMain = () => {
+    navigate('/main-han');
+  };
+
+  return (
+    <div className="login">
+      <form className="loginBox">
+        <h1>Westagram</h1>
+        <input
+          className="userId"
+          type="text"
+          placeholder="전화번호, 사용자 이름 또는 이메일"
+          onChange={handleIdInput}
+        />
+        <input
+          className="userPassword"
+          type="password"
+          placeholder="비밀번호"
+          onChange={handleIdInput}
+        />
+        <button className="loginButton" onClick={goToMain}>
+          로그인
+        </button>
+        <div className="findPassword">
+          <a href="#">비밀번호를 잊으셨나요?</a>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
--- src/pages/younghyunHan/Main/Main.js
+++ src/pages/younghyunHan/Main/Main.js
@@ -1,8 +1,184 @@
 import './Main.scss';
+import Nav from '../../../components/Nav/Nav';
 import '../../../styles/variables.scss';
 
 function Main() {
-  return <></>;
+  return (
+    <div className="main">
+      <Nav />
+      <main>
+        <div className="feeds">
+          <article>
+            <div className="feedTop">
+              <div className="feedTopOne">
+                <div className="profileLogo">wecode</div>
+                <div className="profileName">
+                  <div className="profileNameOne">wecode_bootcamp</div>
+                  <span className="profileNameTwo">Wecode - 위코드</span>
+                </div>
+              </div>
+              <i className="fas fa-ellipsis-h" />
+            </div>
+            <img
+              alt="피드 이미지"
+              className="feedImg"
+              src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            />
+            <div className="feedThree">
+              <div>
+                <i className="far fa-heart" />
+                <i className="far fa-comment" />
+                <i className="fas fa-upload" />
+              </div>
+              <i className="far fa-bookmark" />
+            </div>
+            <div className="feedFour">
+              <img
+                alt="좋아요 누른 사람"
+                className="man"
+                src="https://media.istockphoto.com/photos/smiling-mixed-race-mature-man-on-grey-background-picture-id1319763895?s=612x612"
+              />
+              <span className="feedFourWord">seungyoun_iain</span>
+              &nbsp;외&nbsp;
+              <span className="feedFourWordTwo">4명</span>이 좋아합니다.
+            </div>
+            <div className="feedFive">
+              <span className="feedFiveWord">wecode_bootcamp</span>"위코드는
+              단순 교육업체가 아닌 개발자 커뮤니티입니다. Wecode에서 배우고 저는
+              총 5개 회사에서 오퍼를 받았습니다.
+            </div>
+            <span className="postTime">54분 전</span>
+            <div className="commentSection">
+              <input
+                type="text"
+                placeholder="댓글 달기..."
+                className="comment"
+              />
+              <button className="postButton">게시</button>
+            </div>
+          </article>
+        </div>
+        <div className="mainRight">
+          <div className="sideOne">
+            <div className="profileLogoTwo">wecode</div>
+            <div className="profileName">
+              <div className="profileNameThree">wecode_bootcamp</div>
+              <span className="profileNameFour">Wecode | 위코드</span>
+            </div>
+          </div>
+          <div className="sideTwo">
+            <div className="friendStory">
+              <div className="story">스토리</div>
+              <div className="viewAll">모두 보기</div>
+            </div>
+            <div className="friendStoryNumber">
+              <div className="friendStoryOne">
+                <img
+                  alt="친구1"
+                  className="friendOne"
+                  src="https://media.istockphoto.com/photos/happy-to-travel-again-picture-id1305828428?b=1&k=20&m=1305828428&s=170667a&w=0&h=5SAGDam9ySPJRYW4BGoh9EkGboiQ76dLDGluh63hJhw="
+                />
+                <div>
+                  <div className="friendNameOne">_yum_s</div>
+                  <span className="uploadTimeOne">16분 전</span>
+                </div>
+              </div>
+              <div className="friendStoryTwo">
+                <img
+                  alt="친구2"
+                  className="friendTwo"
+                  src="https://media.istockphoto.com/photos/young-man-arms-outstretched-by-the-sea-at-sunrise-enjoying-freedom-picture-id1285301614?b=1&k=20&m=1285301614&s=170667a&w=0&h=tDEC2-p91cZiNU5C19sVhB9L08PmaH5wIijCvRDalCI="
+                />
+                <div>
+                  <div className="friendNameTwo">drink_eat_drink</div>
+                  <span className="uploadTimeTwo">3시간 전</span>
+                </div>
+              </div>
+              <div className="friendStoryThree">
+                <img
+                  alt="친구3"
+                  className="friendThree"
+                  src="https://media.istockphoto.com/photos/woman-walking-on-a-path-by-the-sea-holding-a-friends-hand-picture-id1288078486?b=1&k=20&m=1288078486&s=170667a&w=0&h=_Hh6hjBsu4hxPhphKow6kk8YHaqtqYFCRRsadwnvRRY="
+                />
+                <div>
+                  <div className="friendNameThree">hyukyc</div>
+                  <span className="uploadTimeThree">20시간 전</span>
+                </div>
+              </div>
+              <div className="friendStoryFour">
+                <img
+                  alt="친구4"
+                  className="friendFour"
+                  src="https://media.istockphoto.com/photos/couple-pushing-trolley-with-their-child-at-airport-picture-id1325212877?b=1&k=20&m=1325212877&s=170667a&w=0&h=lEey74nmiKVFn2Nc0vVUNUhKysbTbiJnyMn3uv013uM="
+                />
+                <div>
+                  <div className="friendNameFour">jminkeek</div>
+                  <span className="uploadTimeFour">10시간 전</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="sideThree">
+            <div className="recommendStory">
+              <div className="recommend">회원님을 위한 추천</div>
+              <div className="viewAll">모두 보기</div>
+            </div>
+            <div className="friendrecommendNumber">
+              <div className="friendRecommendOne">
+                <img
+                  alt="친구5"
+                  className="friendFive"
+                  src="https://media.istockphoto.com/photos/portrait-of-two-happy-black-guys-embracing-while-posing-over-beige-picture-id1337920042?b=1&k=20&m=1337920042&s=170667a&w=0&h=PdoIL9qrqZ9ASgeO03PHopEPIQEbshWr9Aks2aeEoiQ="
+                />
+                <div>
+                  <div className="friendNameFive">joaaaaaahye</div>
+                  <span className="uploadTimeOne">_legend_a님 외 2명이...</span>
+                </div>
+              </div>
+              <div className="friendRecommendTwo">
+                <img
+                  alt="친구6"
+                  className="friendSix"
+                  src="https://media.istockphoto.com/photos/two-black-woman-walking-through-a-grass-field-picture-id1300214955?b=1&k=20&m=1300214955&s=170667a&w=0&h=144AXl3crU7paS1BWMc-fgTDdWgL3gpWgBas8HP6Krg="
+                />
+                <div>
+                  <div className="friendNameSix">rampart81</div>
+                  <span className="uploadTimeTwo">
+                    ringo.in.seoul님 외 12...
+                  </span>
+                </div>
+              </div>
+              <div className="friendRecommendThree">
+                <img
+                  alt="친구7"
+                  className="friendSeven"
+                  src="https://media.istockphoto.com/photos/multiracial-senior-women-having-fun-together-after-sport-workout-picture-id1312102723?b=1&k=20&m=1312102723&s=170667a&w=0&h=jPV0YWw2qL7NdJKKzyTWy3WpOlldMG3uOXPDthuymLI="
+                />
+                <div>
+                  <div className="friendNameSeven">shawnjjoo</div>
+                  <span className="uploadTimeThree">
+                    jimmylee1220님 외 1...
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="companyInformation">
+            <div className="companyInformationOne">
+              Instagram 정보 · 지원 · 홍보 센터 · API ·
+            </div>
+            <div className="companyInformationTwo">
+              채용 정보 · 개인정보처리방침 · 약관 ·
+            </div>
+            <div className="companyInformationThree">
+              디렉토리 · 프로필 · 해시태그 · 언어
+            </div>
+            <div className="companyInformationFour">2022 INSTAGRAM</div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default Main;
