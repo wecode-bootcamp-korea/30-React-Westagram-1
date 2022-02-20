2e53642de0462a4bf2741949fc2da4d80de8dd0c
Bueong <pcj0308@gmail.com>
2022-02-20 14:16:10 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/cheoljinPark/Login/Login.js
+++ src/pages/cheoljinPark/Login/Login.js
@@ -5,11 +5,19 @@ import LoginForm from './LoginForm';
 function Login() {
   let [loginId, setLoginId] = useState('');
   let [loginPw, setLoginPw] = useState('');
+  let [loginCheck, setLoginCheck] = useState(false);
 
   return (
     <div className="Login">
       <header className="logo">Westagram</header>
-      <LoginForm loginId={setLoginId} loginPw={setLoginPw} />
+      <LoginForm
+        loginId={loginId}
+        loginPw={loginPw}
+        loginCheck={loginCheck}
+        setLoginId={setLoginId}
+        setLoginPw={setLoginPw}
+        setLoginCheck={setLoginCheck}
+      />
       <footer className="search-passwd">
         <a href="#">비밀번호를 잊으셨나요?</a>
       </footer>
--- src/pages/cheoljinPark/Login/LoginForm.js
+++ src/pages/cheoljinPark/Login/LoginForm.js
@@ -2,20 +2,31 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './LoginForm.scss';
 
-function LoginForm({ setLoginId, setLoginPw }) {
+function LoginForm({
+  loginId,
+  loginPw,
+  loginCheck,
+  setLoginId,
+  setLoginPw,
+  setLoginCheck,
+}) {
   const navigate = useNavigate();
 
-  const goToMain = () => {
-    navigate('/main-park');
-  };
-
   const handleIdInput = event => {
-    setLoginId = event.target.value;
+    setLoginId(event.target.value);
   };
   const handlePwInput = event => {
-    setLoginPw = event.target.value;
+    setLoginPw(event.target.value);
   };
 
+  const loginInspector = e => {
+    loginId.includes('@') && loginPw.length > 4
+      ? setLoginCheck(true)
+      : setLoginCheck(false);
+  };
+
+  const sendMain = e =>
+    loginCheck ? navigate('/main-park') : alert('정신 차리세요');
   return (
     <form className="login-form">
       <input
@@ -23,14 +34,24 @@ function LoginForm({ setLoginId, setLoginPw }) {
         className="login-id loginInput"
         placeholder="전화번호, 사용자 이름 또는 이메일"
         onChange={handleIdInput}
+        onKeyUp={loginInspector}
       />
       <input
         type="password"
         className="login-pw loginInput"
         placeholder="비밀번호"
         onChange={handlePwInput}
+        onKeyUp={loginInspector}
       />
-      <button className="login-btn" onClick={goToMain}>
+      <button
+        className="login-btn"
+        onClick={sendMain}
+        style={
+          loginCheck
+            ? { opacity: 1, cursor: 'pointer' }
+            : { opacity: 0.5, cursor: 'not-allowed' }
+        }
+      >
         로그인
       </button>
     </form>
